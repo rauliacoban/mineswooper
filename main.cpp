213d0b3bc74e8b7bf47ed6b45b717b0247d2b0a2
rauliacoban <iacoban.raul08@gmail.com>
2022-01-15 13:04:14 +0200

    made main usable to users

--- main.cpp
+++ main.cpp
@@ -2,14 +2,40 @@
 #include "game.h"
 #include "player.h"
 #include <iostream>
+#include <string>
 
-int main()
+#include <windows.h>
+
+int main(int argc, char **argv)
 {
-    const size_t N = 4;
-    const size_t M = 12;
-    const size_t MINES = 7;
-    const size_t SEED = 2623;
-
+    size_t N, M, MINES, SEED;
+    if(argc == 1)
+    {
+        N = 15;
+        M = 31;
+        MINES = 99;
+        SEED = 0;
+    }
+    else if(argc == 2)
+    {
+        SEED = atoi(argv[1]);
+    }
+    else if(argc == 5)
+    {
+        SEED = atoi(argv[1]);
+        N = atoi(argv[2]);
+        M = atoi(argv[3]);
+        MINES = atoi(argv[4]);
+    }
+    else
+    {
+        std::cout << "Provide in the command line when launching the program:\n";
+        std::cout << "a seed for generating a game with standard settings OR\n";
+        std::cout << "a seed, height of board, width of board and number of mines OR\n";
+        std::cout << "leave empty for standard settings\n";
+        exit(1);
+    }
+    
     Game game(N, M, MINES, SEED);
     //game.print(GameCell::symbolWithCover);
     //board.printDebug();
@@ -17,101 +43,38 @@ int main()
     Player player(N, M);
 
     Solution x = Solution(game.getStart(), FREE);
+    Solution sol;
+    bool is_over = 0;
+    int skip = 0;
+
     while(x.isValid())
     {
-        std::cout << x.id << " " << (int)x.val << "\n";
+        sol = x;
         x = game.evaluate(x);
-        player.getInfo(x);
-        //player.print(Cell::getSymbol);
-        game.print(GameCell::symbolWithCover);
+        player.getInfo(x);;
         x = player.getSol();
+        
+        if(skip == 0)
+        {
+            game.print(GameCell::symbolWithCover);
+            std::cout << "Press enter to advance 1 move or enter the number of moves to advance by\n";
+            std::string str = "";
+            std::getline(std::cin, str);
+            if(str.empty())
+            {
+                skip = 1;
+            }
+            else
+            {
+                skip = stoi(str);
+            }
+        }
+        skip--;
     }
+    game.print(GameCell::symbolWithCover);
 
-    PlayerCell *cell = &player.board[1];
-    std::cout << (int)cell->weight << " partial " << cell->id << ": ";
-    for(auto it = cell->partial.begin(); it != cell->partial.end(); it++)
-    {
-        PlayerCell *c = *it;
-        player.printCoords(c);
-    }
-    std::cout << "\n";
+    std::cout << "I can't make any more moves\n seed: " << SEED << "\n";
 
-    cell = &player.board[13];
-    std::cout << (int)cell->weight << " partial " << cell->id << ": ";
-    for(auto it = cell->partial.begin(); it != cell->partial.end(); it++)
-    {
-        PlayerCell *c = *it;
-        player.printCoords(c);
-    }
-    std::cout << "\n";
 
-    cell = &player.board[25];
-    std::cout << (int)cell->weight << " partial " << cell->id << ": ";
-    for(auto it = cell->partial.begin(); it != cell->partial.end(); it++)
-    {
-        PlayerCell *c = *it;
-        player.printCoords(c);
-    }
-    std::cout << "\n";
-
-    cell = &player.board[37];
-    std::cout << (int)cell->weight << " partial " << cell->id << ": ";
-    for(auto it = cell->partial.begin(); it != cell->partial.end(); it++)
-    {
-        PlayerCell *c = *it;
-        player.printCoords(c);
-    }
-    std::cout << "\n";
-
-/*
-    player.board[0].val = INVALID;
-
-    player.board[2].val = UNKNOWN;
-    player.board[4].val = UNKNOWN;
-    player.board[8].val = UNKNOWN;
-    player.board[10].val = UNKNOWN;
-    player.board[6].val = INVALID;
-    player.board[7].val = INVALID;
-    player.board[9].val = INVALID;
-    player.board[11].val = INVALID;
-    player.board[1].val = 1;
-    player.board[3].val = 2;
-    player.board[5].val = 1;
-
-    player.print(PlayerCell::getSymbol);
-
-    player.board[1].setPartial();
-    //player.board[3].setPartial();
-    player.board[5].setPartial();
-    player.board[3].setPartial();
-    //player.board[1].setPartial();
-
-    PlayerCell *cell = &player.board[5];
-    std::cout << (int)cell->weight << " partial 5: ";
-    for(auto it = cell->partial.begin(); it != cell->partial.end(); it++)
-    {
-        PlayerCell *c = *it;
-        player.printCoords(c);
-    }
-    std::cout << "\n";
-
-    cell = &player.board[3];
-    std::cout << (int)cell->weight << " partial 3: ";
-    for(auto it = cell->partial.begin(); it != cell->partial.end(); it++)
-    {
-        PlayerCell *c = *it;
-        player.printCoords(c);
-    }
-    std::cout << "\n";
-
-    cell = &player.board[1];
-    std::cout << (int)cell->weight << " partial 1: ";
-    for(auto it = cell->partial.begin(); it != cell->partial.end(); it++)
-    {
-        PlayerCell *c = *it;
-        player.printCoords(c);
-    }
-    std::cout << "\n";
-*/
     return 0;
 }
